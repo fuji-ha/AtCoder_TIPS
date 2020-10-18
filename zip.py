6438b69544a86a133a35c4250760847a52e57398
haruno-f <enlighten99@gmail.com>
2020-10-18 21:34:09 +0900

    追加2

--- zip.py
+++ zip.py
@@ -1,9 +1,7 @@
-names = ['Alice', 'Bob', 'Charlie']
-ages = [24, 50, 19]
+x = [1,2,3]
+y = [4,5,6]
+print(list(zip(x, y)))
 
-for name, age in zip(names, ages):
-    print(name, age)
+for i,j in zip(x, y):
+    print(i,j)
 
-# Alice 24
-# Bob 50
-# Charlie 18
