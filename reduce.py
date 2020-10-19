b0b93fc873ec43295413909c0e714d4d9eefbb7b
haruno-f <enlighten99@gmail.com>
2020-10-19 22:52:35 +0900

    追加3

--- reduce.py
+++ reduce.py
@@ -1,5 +1,6 @@
 from functools import reduce
 
+# 利用例1
 def func(x, y):
     return x * y
 
@@ -7,5 +8,6 @@ list_a = [100, 20, 30,10]
 
 print(reduce(func, list_a))
 
-b= reduce(lambda x, y: x - y, list_a)
+#利用例2
+b= reduce(lambda x, y: x + y, list_a)
 print(b)
